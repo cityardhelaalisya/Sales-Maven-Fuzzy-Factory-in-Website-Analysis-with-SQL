c48a2b6931f3d0fd28eb890932db55499165a34c
City Ardhela Alisya <100455516+cityardhelaalisya@users.noreply.github.com>
2025-09-05 11:23:00 +0700

    Delete case_1.sql

--- case_1.sql
+++
@@ -1,8 +0,0 @@
--- Which traffic source drives the most visitors to the website?
-select utm_source, utm_campaign, http_referer, count(distinct website_session_id) as sessions
-from website_sessions
-where created_at > '2013-06-01'
-group by 1, 2, 3
-order by 4 desc;
-
--- The result shows that the source is Gsearch with the Nonbrand campaign, which generated 173,897 sessions
